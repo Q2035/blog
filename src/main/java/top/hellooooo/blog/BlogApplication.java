37b52b9a93fbb485c1cbe958e31b158f5e7530de
Q2035 <2383862181@qq.com>
2020-11-12 22:52:25 +0800

    Add loginlog

--- src/main/java/top/hellooooo/blog/BlogApplication.java
+++ src/main/java/top/hellooooo/blog/BlogApplication.java
@@ -3,6 +3,8 @@ package top.hellooooo.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import top.hellooooo.blog.util.LoginLog;
 
 @MapperScan("top.hellooooo.blog.mapper")
 @SpringBootApplication
@@ -10,6 +12,15 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+    }
+
+    /**
+     * 将LoginLog加入容器
+     * @return
+     */
+    @Bean
+    public LoginLog loginLog(){
+        return new LoginLog();
     }
 
 }
--- src/main/java/top/hellooooo/blog/controller/admin/LoginController.java
+++ src/main/java/top/hellooooo/blog/controller/admin/LoginController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import top.hellooooo.blog.util.LoginLog;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Calendar;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/admin")
@@ -20,36 +23,61 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @GetMapping({"","login"})
-    public String loginPage(HttpServletRequest request){
+    @GetMapping({"", "login"})
+    public String loginPage(HttpServletRequest request) {
         HttpSession session = request.getSession();
 //        不同浏览器服务器对应的session不一样，没事儿
-        if (session.getAttribute("user")!=null){
+        if (session.getAttribute("user") != null) {
             return "admin/index";
         }
         return "admin/login";
     }
 
+    /**
+     * 认证
+     *
+     * @param username
+     * @param password
+     * @param request
+     * @param attributes
+     * @return
+     */
     @PostMapping("authentication")
-    public String login(@RequestParam("username")String username,
-                        @RequestParam("password")String password,
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
                         HttpServletRequest request,
-                        RedirectAttributes attributes){
-        HttpSession session = request.getSession();
-        User user = userService.checkUser(username, password);
-        if (user !=null){
-            user.setPassword("");
-            session.setAttribute("user",user);
-            return "admin/index";
-        }else {
-//            如果使用Model，重定向之后页面无法取数据
-            attributes.addFlashAttribute("message","password fail to authenticate");
-            return "redirect:/admin";
+                        RedirectAttributes attributes,
+                        LoginLog loginLog) {
+        // 上次尝试登录距现在超过30分钟则刷新LoginLog
+        Date lastLoginTime = loginLog.getLastLoginTime();
+        Date now = new Date();
+        if (lastLoginTime == null || now.getTime() - lastLoginTime.getTime() > 30 * 60 * 1000) {
+            loginLog.setFailLoginCounts(0);
         }
+        // 登录失败错误次数小于3
+        if (loginLog.getFailLoginCounts() <= 3) {
+            HttpSession session = request.getSession();
+            User user = userService.checkUser(username, password);
+            if (user != null) {
+                user.setPassword("");
+                session.setAttribute("user", user);
+                // 登录成功，刷新登录日志
+                loginLog.setLastLoginTime(now);
+                loginLog.setFailLoginCounts(0);
+                return "admin/index";
+            } else {
+                // 如果使用Model，重定向之后页面无法取数据
+                attributes.addFlashAttribute("message", "Password fail to authenticate");
+                loginLog.increaseFailLoginCount();
+                return "redirect:/admin";
+            }
+        }
+        attributes.addFlashAttribute("message", "Too many attempts.");
+        return "redirect:/admin";
     }
 
     @GetMapping("/logout")
-    public String logout(HttpSession session){
+    public String logout(HttpSession session) {
         session.removeAttribute("user");
         return "redirect:/admin";
     }
